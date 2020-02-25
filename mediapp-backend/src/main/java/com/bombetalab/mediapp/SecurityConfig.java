e6f516eb9b24a8e61204c5b8883da36498d15e78
Oscar Mauricio Morera <Oscar.MoreraLizano@wu.com>
2020-02-25 09:50:29 -0600

    Update SecurityConfig.java

--- mediapp-backend/src/main/java/com/bombetalab/mediapp/SecurityConfig.java
+++ mediapp-backend/src/main/java/com/bombetalab/mediapp/SecurityConfig.java
@@ -18,8 +18,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableWebSecurity
@@ -76,8 +76,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new JwtTokenStore(accessTokenConverter());
-		// return new JdbcTokenStore(this.dataSource);
+		//memory
+		//return new JwtTokenStore(accessTokenConverter());
+		
+		//on DB
+		 return new JdbcTokenStore(this.dataSource);
 	}
 
 	@Bean
